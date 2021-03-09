3bd958bd7a1cf7c0350281cd71aacfe60de880ee
mkjodhani <mkjodhani133@gmail.com>
2021-03-09 13:26:45 +0530

    solve the Font Problem

--- Installer/Installer/src/sample/Main.java
+++ Installer/Installer/src/sample/Main.java
@@ -22,6 +22,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.stage.Stage;
 
 import java.io.IOException;
